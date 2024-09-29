9eab9eef43dbbdd786245a7ee77b0650f78d852f
Tania-Waseem <165193599+TaniaWaseem@users.noreply.github.com>
2024-09-29 23:49:13 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/assignment01_quizapp/StartupPage.java
@@ -0,0 +1,28 @@
+package com.example.assignment01_quizapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StartupPage extends AppCompatActivity {
+
+    Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_page);
+        btnStart = (Button)findViewById(R.id.StartButton);
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(StartupPage.this, QuizApp.class);
+            startActivity(intent);
+        });
+
+    }
+}
